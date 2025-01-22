bac5ad59c1dfcd50d42f7195d1a4c8b33a4a5467
danyla1203 <zelenko.danil@gmail.com>
2025-01-22 15:25:42 +0100

    ready to update logic

--- src/modules/bookingfrontend/client/src/components/event/editing/editing-controller.tsx
+++ src/modules/bookingfrontend/client/src/components/event/editing/editing-controller.tsx
@@ -14,10 +14,29 @@ interface EventEditingProps {
 
 const EventEditing: FC<EventEditingProps> = ({ event, saveChanges, cancelEditing }: EventEditingProps) => {
     const [draft, setDraft] = useState(event);
+    const [readyToUpdate, setStatus] = useState(false);
     const t = useTrans();
 
     const updateField = (key: keyof ActivityData, value: string | number) => {
-        const copy = {...draft, [key]: value}
+        const copy = {...draft, [key]: value};
+        if (JSON.stringify(copy) !== JSON.stringify(event)) setStatus(true);
+        else setStatus(false);
+        if (key === 'resources' && !readyToUpdate) {
+            const newResources = copy['resources'];
+            const oldResources = event['resources'];
+            if (newResources.size !== oldResources.size) {
+                setStatus(true);
+            } else {
+                let flag = false;
+                for (const key of newResources.keys()) {
+                    if (!oldResources.has(key)) {
+                        flag = true;
+                        break;
+                    }
+                }
+                setStatus(flag);
+            }
+        }
         setDraft(copy);
     }
 
@@ -30,7 +49,7 @@ const EventEditing: FC<EventEditingProps> = ({ event, saveChanges, cancelEditing
                     onClick={cancelEditing}
                     style={{ marginRight: '0.5rem' }}
                 >{t('bookingfrontend.cancel')}</Button>
-                <Button onClick={() => saveChanges(draft)}>{t('bookingfrontend.save')}</Button>
+                <Button disabled={!readyToUpdate} onClick={() => saveChanges(draft)}>{t('bookingfrontend.save')}</Button>
             </div>
         </main>
     )

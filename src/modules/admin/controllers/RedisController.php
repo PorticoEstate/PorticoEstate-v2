36adfad01575cc220495da9014e1b0b1d99ee27b
Sigurd Nes <sigurdne@gmail.com>
2025-01-23 10:18:09 +0100

    Add Redis connection check in RedisController

--- src/modules/admin/controllers/RedisController.php
+++ src/modules/admin/controllers/RedisController.php
@@ -45,6 +45,12 @@ class RedisController
 		$phpgwapi_common = new \phpgwapi_common();
 		$phpgwapi_common->phpgw_header(true);
 
+		if (!$this->redis)
+		{
+			$response->getBody()->write('Redis is not enabled or not connected');
+			return $response;
+		}
+
 		// Fetch memory usage
 		$memoryUsage = $this->redis->info('memory')['used_memory'];
 

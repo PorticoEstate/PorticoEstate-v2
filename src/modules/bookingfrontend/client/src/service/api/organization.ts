a79a2f0434f19fc89ab2bde5b11c2fbb92706140
danyla1203 <zelenko.danil@gmail.com>
2025-02-18 14:46:44 +0100

    create and get delegate requests

--- src/modules/bookingfrontend/client/src/service/api/organization.ts
+++ src/modules/bookingfrontend/client/src/service/api/organization.ts
@@ -1,6 +1,7 @@
-import {useQuery, UseQueryResult} from "@tanstack/react-query";
+import {useQuery, UseQueryResult, useMutation} from "@tanstack/react-query";
 import {phpGWLink} from "@/service/util";
 import { Organization } from "../types/api/organization.types";
+import { CreatingDelegate } from "@/components/organization/delegate/schemas";
 
 export const useOrganizationData = (orgId: number) => {
     return useQuery({
@@ -16,7 +17,52 @@ export const useOrganizationData = (orgId: number) => {
                 delegaters: JSON.parse(data.delegaters)
             }
         }
+    });
+}
+
+export const useDelegateData = (delegateId: number) => {
+    return useQuery({
+        queryKey: ['delegate', delegateId],
+        retry: 2,
+        queryFn: async (): Promise<Organization> => {
+            const url = phpGWLink([
+                'bookingfrontend', 
+                'organization',
+                'delegate', 
+                delegateId
+            ]);
+            const res = await fetch(url);
+            const data = await res.json();
+            return {
+                ...data,
+                groups: JSON.parse(data.groups),
+                delegaters: JSON.parse(data.delegaters)
+            }
+        }
     })
 }
 
-export { Organization };
+export const createDelegate = (orgId: number) => {
+    return useMutation({
+        mutationFn: async (data: CreatingDelegate) => {
+            const url = phpGWLink([
+                'bookingfrontend', 
+                'organization', 
+                orgId, 
+                'delegate'
+            ]);
+            const res = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+
+            if (!res.ok) {
+                throw new Error();
+            }
+            return await res.json();
+        }
+    });
+}

b04dd3b6aa4108d5f26ac49c33528a89b959e80a
Henning Berge <henning.ber@gmail.com>
2025-04-03 13:26:23 +0200

    link fixes mobile

--- src/modules/bookingfrontend/client/src/app/[lang]/(public)/user/user-page-client.tsx
+++ src/modules/bookingfrontend/client/src/app/[lang]/(public)/user/user-page-client.tsx
@@ -13,7 +13,7 @@ interface UserPageClientProps {
 const UserPageClient: FC<UserPageClientProps> = (props) => {
     const t = useTrans();
     const user = useBookingUser();
-    
+
     const links = useMemo(() => {
         return userSubPages.filter(a => !a.needsDelegates || (user.data?.delegates?.length || 0) > 0);
     }, [user]);
@@ -22,21 +22,23 @@ const UserPageClient: FC<UserPageClientProps> = (props) => {
         <main>
             <PageHeader title={t('bookingfrontend.my page')}/>
 
-            <section>
+            <section style={{display: 'flex', flexDirection: 'column', gap: '0.5rem'}}>
                 {links.map((link, index) => {
                     const SVGIcon = link.icon;
                     const fullPath = '/user' + link.relativePath;
-                    
+
                     return (
                         <Card
                             key={index}
                             asChild
                             color="neutral"
                         >
-                            <Link href={fullPath}>
+                            <Link href={fullPath} className={'no-decoration'}>
                                 <Heading
                                     level={2}
                                     data-size="sm"
+									className={'text-primary'}
+
                                 >
                                     <span style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
                                         <SVGIcon fontSize='1.75rem' aria-hidden />

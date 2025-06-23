5dc50596a37d6aef0f695a391ecd77d107233132
Henning Berge <henning.ber@gmail.com>
2025-06-23 09:26:20 +0200

    expose simplebooking start date

--- src/modules/bookingfrontend/client/src/components/building-calendar/util/calender-helpers.ts
+++ src/modules/bookingfrontend/client/src/components/building-calendar/util/calender-helpers.ts
@@ -1,5 +1,6 @@
 import { IResource } from "@/service/types/resource.types";
+import {IShortResource} from "@/service/pecalendar.types";
 
-export const ResourceUsesTimeSlots = (resource: IResource): boolean => {
+export const ResourceUsesTimeSlots = (resource: IResource | IShortResource): boolean => {
 	return resource.simple_booking === 1 && resource.simple_booking_start_date !== null;
 }

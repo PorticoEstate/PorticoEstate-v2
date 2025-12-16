d080038eac4858db351fd708ea1afc4e2ce21ae4
Sigurd Nes <sigurdne@gmail.com>
2025-12-16 15:37:09 +0100

    datatype

--- src/modules/property/inc/class.uitenant_claim.inc.php
+++ src/modules/property/inc/class.uitenant_claim.inc.php
@@ -1213,9 +1213,9 @@ class property_uitenant_claim extends phpgwapi_uicommon_jquery
 				if ($project_values['workorder_budget'][$d]['selected'] == 1)
 				{
 
-					$project_values['workorder_budget'][$d]['budget_hidden']		 = $project_values['workorder_budget'][$d]['budget'];
-					$project_values['workorder_budget'][$d]['calculation_hidden']	 = $project_values['workorder_budget'][$d]['calculation'];
-					$project_values['workorder_budget'][$d]['actual_cost_hidden']	 = $project_values['workorder_budget'][$d]['actual_cost'];
+					$project_values['workorder_budget'][$d]['budget_hidden']		 = (int)$project_values['workorder_budget'][$d]['budget'];
+					$project_values['workorder_budget'][$d]['calculation_hidden']	 = (int)$project_values['workorder_budget'][$d]['calculation'];
+					$project_values['workorder_budget'][$d]['actual_cost_hidden']	 = (int)$project_values['workorder_budget'][$d]['actual_cost'];
 					$project_values['workorder_budget'][$d]['selected']				 = "<input type=\"checkbox\" name=\"values[{$type}][]\" checked value=\"" . $project_values['workorder_budget'][$d]['workorder_id'] . '">';
 				}
 				else

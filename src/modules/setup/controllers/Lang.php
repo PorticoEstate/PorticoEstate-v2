348f43975f878ad2613b1e3b21a487bcce8d7400
Sigurd Nes <sigurdne@gmail.com>
2026-02-02 14:05:12 +0100

    revert temporary

--- src/modules/setup/controllers/Lang.php
+++ src/modules/setup/controllers/Lang.php
@@ -22,7 +22,6 @@ use App\modules\phpgwapi\services\setup\Html;
 use App\helpers\Template;
 use App\modules\phpgwapi\services\setup\SetupTranslation;
 use App\modules\phpgwapi\services\Sanitizer;
-use App\modules\phpgwapi\services\Twig;
 use PDO;
 
 class Lang
@@ -35,11 +34,12 @@ class Lang
 	private $process;
 	private $html;
 	private $setup;
+	private $setup_tpl;
 	private $translation;
-	private $twig;
 
 	public function __construct()
 	{
+
 		//setup_info
 		Settings::getInstance()->set('setup_info', []); //$GLOBALS['setup_info']
 		//setup_data
@@ -51,16 +51,16 @@ class Lang
 		$this->html = new Html();
 		$this->setup = new Setup();
 		$this->translation = new SetupTranslation();
-		$this->twig = Twig::getInstance();
 
 		$flags = array(
 			'noheader' 		=> True,
 			'nonavbar'		=> True,
-			'currentapp'	=> 'setup',
+			'currentapp'	=> 'home',
 			'noapi'			=> True,
 			'nocachecontrol' => True
 		);
 		Settings::getInstance()->set('flags', $flags);
+
 
 		// Check header and authentication
 		if (!$this->setup->auth('Config'))
@@ -69,10 +69,16 @@ class Lang
 			exit;
 		}
 
+		$tpl_root = $this->html->setup_tpl_dir('setup');
+		$this->setup_tpl = new Template($tpl_root);
+
+		$this->html->set_tpl($this->setup_tpl);
 	}
+
 
 	public function index()
 	{
+
 		if (\Sanitizer::get_var('cancel', 'bool', 'POST'))
 		{
 			Header('Location: ../setup');
@@ -80,6 +86,7 @@ class Lang
 		}
 
 		$newinstall = false;
+
 
 		if (isset($_POST['submit']) && $_POST['submit'])
 		{
@@ -95,8 +102,13 @@ class Lang
                             <h2>ERROR</h2>
                             $error
                         </div>
-        
-HTML;
+
+        HTML;
+
+				$this->setup_tpl->set_file(array(
+					'T_head'		=> 'head.tpl',
+					'T_footer'		=> 'footer.tpl',
+				));
 
 				$stage_title = $this->setup->lang('Multi-Language support setup');
 				$stage_desc  = $this->setup->lang('ERROR');
@@ -109,13 +121,13 @@ HTML;
                         <div>
                             <a href="../setup/lang">$return</a>
                         </div>
-        
-HTML;
+
+        HTML;
 				$footer = $this->html->get_footer();
 				return $header . $error . $footer;
 			}
 
-			Header('Location: ../setup/lang');
+			Header('Location: ../setup/lang?success=1');
 			exit;
 		}
 		else
@@ -123,6 +135,25 @@ HTML;
 			$this->detection->check_lang(false);	// get installed langs
 
 			$setup_data = Settings::getInstance()->get('setup');
+
+			$this->setup_tpl->set_file(array(
+				'T_head'		=> 'head.tpl',
+				'T_footer'		=> 'footer.tpl',
+				'T_alert_msg'	=> 'msg_alert_msg.tpl',
+				'T_lang_main'	=> 'lang_main.tpl'
+			));
+
+			$success_message = '';
+			if (\Sanitizer::get_var('success', 'bool', 'GET'))
+			{
+				$success_message = <<<HTML
+					<div style="background-color: #d4edda; border: 1px solid #c3e6cb; color: #155724; padding: 12px; margin: 10px 0; border-radius: 4px;">
+						<strong>Success!</strong> Languages have been installed successfully.
+					</div>
+HTML;
+			}
+
+			$this->setup_tpl->set_block('T_lang_main', 'B_choose_method', 'V_choose_method');
 
 			$stage_title = $this->setup->lang('Multi-Language support setup');
 			$stage_desc  = $this->setup->lang('This program will help you upgrade or install different languages for phpGroupWare');
@@ -162,19 +193,6 @@ HTML;
 
 			$this->db->query("UPDATE phpgw_languages SET available = 'Yes' WHERE lang_id IN('" . implode("','", $avail_lang) . "')");
 
-			// Prepare template variables
-			$templateVars = [
-				'stage_title' => $stage_title,
-				'stage_desc' => $stage_desc,
-				'tbl_width' => $tbl_width,
-				'td_colspan' => $td_colspan,
-				'td_align' => $td_align,
-				'hidden_var1' => $hidden_var1,
-				'select_box_desc' => $select_box_desc,
-				'checkbox_langs' => $checkbox_langs
-			];
-
-			// Add method description and options if not a new install
 			if (!$newinstall)
 			{
 				$meth_desc = $this->setup->lang('Select which method of upgrade you would like to do');
@@ -182,23 +200,35 @@ HTML;
 				$blurb_addmissing = $this->setup->lang('Only add new phrases');
 				$blurb_dumpold = $this->setup->lang('Delete all old languages and install new ones');
 
-				$templateVars['meth_desc'] = $meth_desc;
-				$templateVars['blurb_addonlynew'] = $blurb_addonlynew;
-				$templateVars['blurb_addmissing'] = $blurb_addmissing;
-				$templateVars['blurb_dumpold'] = $blurb_dumpold;
+				$this->setup_tpl->set_var('meth_desc', $meth_desc);
+				$this->setup_tpl->set_var('blurb_addonlynew', $blurb_addonlynew);
+				$this->setup_tpl->set_var('blurb_addmissing', $blurb_addmissing);
+				$this->setup_tpl->set_var('blurb_dumpold', $blurb_dumpold);
+				$this->setup_tpl->parse('V_choose_method', 'B_choose_method');
 			}
+			else
+			{
+				$this->setup_tpl->set_var('V_choose_method', '');
+			}
+
+			$this->setup_tpl->set_var('stage_title', $stage_title);
+			$this->setup_tpl->set_var('stage_desc', $stage_desc);
+			$this->setup_tpl->set_var('tbl_width', $tbl_width);
+			$this->setup_tpl->set_var('td_colspan', $td_colspan);
+			$this->setup_tpl->set_var('td_align', $td_align);
+			$this->setup_tpl->set_var('hidden_var1', $hidden_var1);
+			$this->setup_tpl->set_var('select_box_desc', $select_box_desc);
+			$this->setup_tpl->set_var('checkbox_langs', $checkbox_langs);
+
+			$this->setup_tpl->set_var('lang_install', $this->setup->lang('install'));
+			$this->setup_tpl->set_var('lang_cancel', $this->setup->lang('cancel'));
 
 			$db_config = $this->db->get_config();
 
-			// Get header and footer
-			$header = $this->html->get_header($stage_title, False, 'config', $this->db->get_domain() . '(' . $db_config['db_type'] . ')');
-			
-			// Render the main template using Twig
-			$main = $this->twig->render('lang_main.html.twig', $templateVars);
-			
+			$header = $this->html->get_header("$stage_title", False, 'config', $this->db->get_domain() . '(' . $db_config['db_type'] . ')');
+			$main = $this->setup_tpl->fp('out', 'T_lang_main');
 			$footer = $this->html->get_footer();
-			
-			return $header . $main . $footer;
+			return $header . $success_message . $main . $footer;
 		}
 	}
 }

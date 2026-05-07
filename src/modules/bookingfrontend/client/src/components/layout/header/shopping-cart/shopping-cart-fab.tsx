7dbc4adca01b87f78d664c4c44e5bfb39919e0be
Henning Berge <henning.ber@gmail.com>
2026-05-07 11:02:21 +0200

    Fix new shopping cart mobile

--- src/modules/bookingfrontend/client/src/components/layout/header/shopping-cart/shopping-cart-fab.tsx
+++ src/modules/bookingfrontend/client/src/components/layout/header/shopping-cart/shopping-cart-fab.tsx
@@ -5,9 +5,7 @@ import {Badge, Button} from "@digdir/designsystemet-react";
 import { ShoppingBasketIcon } from "@navikt/aksel-icons";
 import {usePartialApplications} from "@/service/hooks/api-hooks";
 import styles from './shopping-cart-fab.module.scss';
-import ShoppingCartPopper from "@/components/layout/header/shopping-cart/shopping-cart-popper";
 import ShoppingCartDrawer from "@/components/layout/header/shopping-cart/shopping-cart-drawer";
-import {useIsMobile} from "@/service/hooks/is-mobile";
 
 interface ShoppingCartFabProps {
 }
@@ -16,7 +14,6 @@ const ShoppingCartFab: FC<ShoppingCartFabProps> = (props) => {
     const {data: cartItems} = usePartialApplications();
     const [open, setOpen] = useState<boolean>(false);
     const popperAnchorEl = useRef<HTMLButtonElement | null>(null);
-    const isMobile = useIsMobile();
 
     // Check if cart has items
     const hasItems = (cartItems?.list.length ?? 0) > 0;
@@ -49,11 +46,7 @@ const ShoppingCartFab: FC<ShoppingCartFabProps> = (props) => {
                 ></Badge>
             </Button>
 
-            {isMobile ? (
-                <ShoppingCartPopper anchor={popperAnchorEl.current} open={open} setOpen={setOpen}/>
-            ) : (
-                <ShoppingCartDrawer open={open} setOpen={setOpen} />
-            )}
+            <ShoppingCartDrawer open={open} setOpen={setOpen} anchor={popperAnchorEl.current} />
         </div>
     );
 }

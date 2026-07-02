1ea9493d47023bcb8fec8a6117a75c080afb7ccf
Sigurd Nes <sigurdne@gmail.com>
2026-07-02 12:59:41 +0200

    refactor todo user interface: remove dependency on phpgwapi_uicommon_jquery and clean up unused query method

--- src/modules/todo/inc/class.uitodo.inc.php
+++ src/modules/todo/inc/class.uitodo.inc.php
@@ -9,9 +9,8 @@
  * @package todo
  */
 
-phpgw::import_class('phpgwapi.uicommon_jquery');
 
-class todo_uitodo extends phpgwapi_uicommon_jquery
+class todo_uitodo
 {
 	public $public_functions = array(
 		'show_list' => true,
@@ -23,12 +22,9 @@ class todo_uitodo extends phpgwapi_uicommon_jquery
 		'query' => true,
 	);
 
-	private $botodo;
 
 	public function __construct()
 	{
-		parent::__construct('todo');
-		$this->botodo = CreateObject('todo.botodo', true);
 	}
 
 	public function show_list()
@@ -50,62 +46,7 @@ class todo_uitodo extends phpgwapi_uicommon_jquery
 		phpgw::redirect_link('/todo/view/todos', $redirect_params);
 	}
 
-	/**
-	 * Compatibility shim for phpgwapi_uicommon_jquery.
-	 * Todo list data is served by todo.botodo and returned as jquery_results.
-	 */
-	public function query()
-	{
-		$search = Sanitizer::get_var('search');
-		$order = Sanitizer::get_var('order');
-		$columns = Sanitizer::get_var('columns');
 
-		$start = Sanitizer::get_var('start', 'int', 'REQUEST', 0);
-		$length = Sanitizer::get_var('length', 'int', 'REQUEST', 25);
-		$filter = Sanitizer::get_var('filter', 'string', 'REQUEST', 'none');
-		$cat_id = Sanitizer::get_var('cat_id', 'int', 'REQUEST', 0);
-
-		$sort_map = array(
-			'id' => 'todo_id',
-			'title' => 'todo_title',
-			'status' => 'todo_status',
-			'pri' => 'todo_pri',
-			'sdate' => 'todo_startdate',
-			'edate' => 'todo_enddate',
-			'owner' => 'todo_owner',
-		);
-
-		$sort = 'todo_id';
-		$dir = 'ASC';
-
-		if (is_array($order) && isset($order[0]['column']) && isset($columns[$order[0]['column']]['data']))
-		{
-			$column_key = $columns[$order[0]['column']]['data'];
-			if (isset($sort_map[$column_key]))
-			{
-				$sort = $sort_map[$column_key];
-			}
-
-			$order_dir = isset($order[0]['dir']) ? strtolower($order[0]['dir']) : 'asc';
-			$dir = $order_dir === 'desc' ? 'DESC' : 'ASC';
-		}
-
-		$todo_list = $this->botodo->_list(
-			$start,
-			$length,
-			is_array($search) && isset($search['value']) ? $search['value'] : '',
-			$filter,
-			$sort,
-			$dir,
-			$cat_id,
-			'all'
-		);
-
-		return $this->jquery_results(array(
-			'total_records' => $this->botodo->total_records,
-			'results' => is_array($todo_list) ? $todo_list : array(),
-		));
-	}
 
 	public function add()
 	{

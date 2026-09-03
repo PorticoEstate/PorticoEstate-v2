ee68cba31b1ef9eb84272b1fc67a24b5ce523e02
Sigurd Nes <sigurdne@gmail.com>
2026-09-03 13:06:34 +0200

    consolidate: remove obsolete

--- src/modules/phpgwapi/js/DataTables3/vendor/datatables.net/datatables.net-buttons-dt/js/buttons.dataTables.min.mjs
+++ src/modules/phpgwapi/js/DataTables3/vendor/datatables.net/datatables.net-buttons-dt/js/buttons.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! Buttons default styling 4.0.1 for DataTables
+/*! Buttons default styling 4.0.2 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import Buttons from"datatables.net-buttons";export default DataTable;
